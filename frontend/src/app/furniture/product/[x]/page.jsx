4c75c4f6217aa9d19e72e4b25a2244f2143aecd4
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-19 11:58:51 +0300

    Commit

--- frontend/src/app/furniture/product/[x]/page.jsx
+++ frontend/src/app/furniture/product/[x]/page.jsx
@@ -5,12 +5,12 @@ import ProductDetail from "@/app/_components/ProductDetail";
 import { useParams } from "next/navigation";
 import React from "react";
 
-const Page = () => {
+const Page = ({}) => {
   const param = useParams();
   return (
     <div>
       <Header />
-      <ProductDetail id={param.product} />
+      <ProductDetail id={param.x} />
     </div>
   );
 };
