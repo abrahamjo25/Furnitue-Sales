c8281bb4f3004391c0f47ed9b166a81e98629866
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-20 14:25:34 +0300

    commit

--- frontend/src/app/furniture/blogs/page.jsx
+++ frontend/src/app/furniture/blogs/page.jsx
@@ -1,18 +1,18 @@
-import BlogSection from '@/app/_components/BlogSection'
-import Footer from '@/app/_components/Footer'
-import Header from '@/app/_components/Header'
-import Hero from '@/app/_components/Hero'
-import React from 'react'
+import BlogSection from "@/app/_components/BlogSection";
+import Footer from "@/app/_components/Footer";
+import Header from "@/app/_components/Header";
+import Hero from "@/app/_components/Hero";
+import React from "react";
 
 const Page = () => {
   return (
     <div>
-      <Header/>
-      <Hero title="Bolg"/>
-      <BlogSection/>
-      <Footer/>
+      <Header />
+      <Hero title="Bolg" />
+      <BlogSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
